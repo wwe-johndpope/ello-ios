64b008f9a12ab4028ee8e08f2e06650c4a2516c2
Colin T.A. Gray <colinta@gmail.com>
2016-06-21 11:58:11 -0600

    move ProfileViewButtons into separate file

--- Sources/Controllers/Profile/ProfileViewController.swift
+++ Sources/Controllers/Profile/ProfileViewController.swift
@@ -8,35 +8,6 @@
 
 import FLAnimatedImage
 
-public class ProfileButton: RoundedElloButton {
-    override public func sharedSetup() {
-        super.sharedSetup()
-
-        setTitleColor(UIColor.blackColor(), forState: .Normal)
-        setTitleColor(UIColor.grey6(), forState: .Highlighted)
-        setTitleColor(UIColor.greyC(), forState: .Disabled)
-    }
-
-    override func updateOutline() {
-        super.updateOutline()
-        backgroundColor = highlighted ? UIColor.grey4D() : UIColor.whiteColor()
-    }
-}
-
-public class ElloMentionButton: RoundedElloButton {
-    override public func sharedSetup() {
-        super.sharedSetup()
-        setTitle(InterfaceString.Profile.Mention, forState: .Normal)
-    }
-}
-
-public class ElloEditProfileButton: RoundedElloButton {
-    override public func sharedSetup() {
-        super.sharedSetup()
-        setTitle(InterfaceString.Profile.EditProfile, forState: .Normal)
-    }
-}
-
 
 public class ProfileViewController: StreamableViewController {
 
