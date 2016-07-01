446ec80cd8d8afd930f58064e6a836b935c613e4
Colin T.A. Gray <colinta@gmail.com>
2016-07-01 17:00:55 -0600

    order by 'level'

--- Sources/Model/CategoryLevel.swift
+++ Sources/Model/CategoryLevel.swift
@@ -11,6 +11,15 @@ public enum CategoryLevel: String, Equatable {
     case Primary = "primary"
     case Secondary = "secondary"
     case Tertiary = "tertiary"
+
+    var order: Int {
+        switch self {
+        case Meta: return 0
+        case Primary: return 1
+        case Secondary: return 2
+        case Tertiary: return 3
+        }
+    }
 }
 
 public func == (lhs: CategoryLevel, rhs: CategoryLevel) -> Bool {
