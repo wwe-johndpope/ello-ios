193e90477ebf20bdf51c3fa136fd63a1cae192c1
Colin T.A. Gray <colinta@gmail.com>
2016-07-01 16:51:50 -0600

    show secondary categories in 'MoreCategories'

--- Sources/Controllers/Discover/DiscoverMoreCategoriesViewController.swift
+++ Sources/Controllers/Discover/DiscoverMoreCategoriesViewController.swift
@@ -68,7 +68,7 @@ extension DiscoverMoreCategoriesViewController {
         var items: [StreamCellItem] = CategoryList.metaCategories().map { StreamCellItem(jsonable: $0, type: .Category) }
         if let categories = jsonables as? [Category] {
             let sortedCategories = categories
-                .filter { $0.level == .Primary }
+                .filter { $0.level == .Primary || $0.level == .Secondary }
                 .sort { $0.order < $1.order }
             for category in sortedCategories {
                 items.append(StreamCellItem(jsonable: category, type: .Category))
