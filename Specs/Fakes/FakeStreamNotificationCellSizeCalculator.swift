577f930ae57dbf2dc4afc5bd820096ec01d9a2b9
Colin T.A. Gray <colinta@gmail.com>
2016-07-11 14:17:50 -0600

    updated Fake Calculators in Specs

--- Specs/Fakes/FakeStreamNotificationCellSizeCalculator.swift
+++ Specs/Fakes/FakeStreamNotificationCellSizeCalculator.swift
@@ -8,9 +8,7 @@ import Foundation
 
 public class FakeStreamNotificationCellSizeCalculator: StreamNotificationCellSizeCalculator {
 
-    override public func processCells(cellItems:[StreamCellItem], withWidth: CGFloat, columnCount: Int, completion:StreamTextCellSizeCalculated) {
-        self.completion = completion
-        self.cellItems = cellItems
+    override public func processCells(cellItems: [StreamCellItem], withWidth: CGFloat, columnCount: Int, completion: ElloEmptyCompletion) {
         for item in cellItems {
             item.calculatedOneColumnCellHeight = AppSetup.Size.calculatorHeight
             item.calculatedMultiColumnCellHeight = AppSetup.Size.calculatorHeight
--- Specs/Fakes/FakeStreamTextCellSizeCalculator.swift
+++ Specs/Fakes/FakeStreamTextCellSizeCalculator.swift
@@ -7,9 +7,7 @@ import Ello
 
 public class FakeStreamTextCellSizeCalculator: StreamTextCellSizeCalculator {
 
-    override public func processCells(cellItems:[StreamCellItem], withWidth: CGFloat, columnCount: Int, completion:StreamTextCellSizeCalculated) {
-        self.completion = completion
-        self.cellItems = cellItems
+    override public func processCells(cellItems: [StreamCellItem], withWidth: CGFloat, columnCount: Int, completion: ElloEmptyCompletion) {
         for item in cellItems {
             item.calculatedOneColumnCellHeight = AppSetup.Size.calculatorHeight
             item.calculatedMultiColumnCellHeight = AppSetup.Size.calculatorHeight
