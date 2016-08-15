7e478943606dbc1ec2b81c39338187d754185c3c
Colin T.A. Gray <colinta@gmail.com>
2016-08-15 08:50:43 -0600

    prefer 'guard'

--- Sources/Controllers/Omnibar/OmnibarScreen.swift
+++ Sources/Controllers/Omnibar/OmnibarScreen.swift
@@ -426,11 +426,11 @@ public class OmnibarScreen: UIView, OmnibarScreenProtocol {
     }
 
     public func profileImageTapped() {
-        if let currentUser = currentUser {
-            let profileVC = ProfileViewController(user: currentUser)
-            profileVC.currentUser = currentUser
-            self.delegate?.omnibarPushController(profileVC)
-        }
+        guard let currentUser = currentUser else { return }
+
+        let profileVC = ProfileViewController(user: currentUser)
+        profileVC.currentUser = currentUser
+        self.delegate?.omnibarPushController(profileVC)
     }
 
     // called on a user action that should resign the keyboard
