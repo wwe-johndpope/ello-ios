9e3b2e85cbf48584263567add85e46716d5745ff
Colin T.A. Gray <colinta@gmail.com>
2016-09-26 15:44:03 -0600

    CategoriesSelectionViewControllerSpec

---
+++ Specs/Controllers/Onboarding/CategoriesSelectionViewControllerSpec.swift
@@ -0,0 +1,34 @@
+////
+///  CategoriesSelectionViewControllerSpec.swift
+//
+
+@testable import Ello
+import Quick
+import Nimble
+
+
+class CategoriesSelectionViewControllerSpec: QuickSpec {
+    override func spec() {
+        var subject: CategoriesSelectionViewController!
+        beforeEach {
+            subject = CategoriesSelectionViewController()
+            subject.onboardingData = OnboardingData()
+        }
+        describe("CategoriesSelectionViewController") {
+            var categories: [Ello.Category]!
+            beforeEach {
+                let category: Ello.Category = stub(["name": "Art"])
+                categories = [category]
+                subject.categoriesSelectionChanged(categories)
+            }
+
+            it("saves selected categories from streamViewController") {
+                expect(subject.selectedCategories) == categories
+            }
+            it("submitting categories saves categories to onboardingData") {
+                subject.onboardingWillProceed(false, proceedClosure: { _ in })
+                expect(subject.onboardingData.categories) == categories
+            }
+        }
+    }
+}
