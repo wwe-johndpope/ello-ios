d2ee411254f273be9118dc362dbab1569d625b0a
Sean Dougherty <seancdougherty@gmail.com>
2016-10-12 15:28:27 -0600

    WIP: adding categories modal

---
+++ Sources/Controllers/Profile/ProfileCategoriesViewController.swift
@@ -0,0 +1,10 @@
+////
+///  ProfileCategoriesViewController.swift
+//
+
+public final class ProfileCategoriesViewController: BaseElloViewController {
+
+    public init(categories: [Category]) {
+        super.init(nibName: nil, bundle: nil)
+    }
+}
