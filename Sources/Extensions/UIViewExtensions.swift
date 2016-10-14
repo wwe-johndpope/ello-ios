4ace6c849287ab1b93292e56922e4fe71dcbdf9d
Sean Dougherty <seancdougherty@gmail.com>
2016-10-14 13:39:43 -0600

    Adds ability to find a parent view of a given type
    An additional test can be passed to the lookup

--- Sources/Extensions/UIViewExtensions.swift
+++ Sources/Extensions/UIViewExtensions.swift
@@ -3,4 +3,15 @@
 //
 
 extension UIView {
+
+    public func findParentView<T where T: UIView>(test: ((T) -> Bool)? = nil) -> T? {
+        var view: UIView? = superview
+        while view != nil {
+            if let view = view as? T where test?(view) ?? true {
+                return view
+            }
+            view = view?.superview
+        }
+        return nil
+    }
 }
