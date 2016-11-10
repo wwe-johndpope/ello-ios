b838a8a5702b6ec669031b54e9fbe5e8f3f02a17
Colin T.A. Gray <colinta@gmail.com>
2016-11-10 15:25:34 -0700

    implements specs for CategoryCardListView and CategoryScreen

---
+++ Specs/Views/CategoryCardListViewSpec.swift
@@ -0,0 +1,50 @@
+////
+///  CategoryCardListViewSpec.swift
+//
+
+@testable import Ello
+import Quick
+import Nimble
+
+
+class CategoryCardListViewSpec: QuickSpec {
+    class MockCategoryCardListDelegate: CategoryCardListDelegate {
+        var selectedIndex: Int?
+        func categoryCardSelected(index: Int) {
+            selectedIndex = index
+        }
+    }
+
+    override func spec() {
+        var subject: CategoryCardListView!
+        var delegate: MockCategoryCardListDelegate!
+        beforeEach {
+            subject = CategoryCardListView(frame: CGRect(origin: .zero, size: CGSize(width: 320, height: CategoryCardListView.Size.height)))
+            let infoA = CategoryCardListView.CategoryInfo(
+                title: "Art",
+                imageURL: nil
+            )
+            let infoB = CategoryCardListView.CategoryInfo(
+                title: "Lorem ipsum dolor sit amet",
+                imageURL: nil
+            )
+            subject.categoriesInfo = [infoA, infoB, infoA, infoB]
+            delegate = MockCategoryCardListDelegate()
+            subject.delegate = delegate
+        }
+
+        fdescribe("CategoryCardListView") {
+            it("should have a valid snapshot") {
+                expectValidSnapshot(subject, named: "CategoryCardListView", device: .Custom(subject.frame.size))
+            }
+
+            describe("CategoryCardListDelegate") {
+                it("informs delegates of category selection") {
+                    let button = subviewThatMatches(subject, test: { $0 is UIButton }) as! UIButton
+                    button.sendActionsForControlEvents(.TouchUpInside)
+                    expect(delegate.selectedIndex) == 0
+                }
+            }
+        }
+    }
+}
