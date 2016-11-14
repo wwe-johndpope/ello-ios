5f789aa6c1d9c5612db6c24392cc6dafc0e963fb
Colin T.A. Gray <colinta@gmail.com>
2016-11-14 11:00:52 -0700

    fixed category var name

--- Sources/Controllers/Profile/Categories/ProfileCategoriesViewController.swift
+++ Sources/Controllers/Profile/Categories/ProfileCategoriesViewController.swift
@@ -41,7 +41,7 @@ extension ProfileCategoriesViewController: UIViewControllerTransitioningDelegate
 extension ProfileCategoriesViewController: ProfileCategoriesDelegate {
 
     public func categoryTapped(category: Category) {
-        Tracker.sharedTracker.categoryOpened(categoryOpened.slug)
+        Tracker.sharedTracker.categoryOpened(category.slug)
         let vc = CategoryViewController(slug: category.slug)
         vc.currentUser = currentUser
 
