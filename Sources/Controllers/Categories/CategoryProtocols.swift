2d44223582ec9b23030131bd0d82147fd5a3edd0
Colin T.A. Gray <colinta@gmail.com>
2016-11-17 14:53:46 -0700

    note to self

--- Sources/Controllers/Categories/CategoryProtocols.swift
+++ Sources/Controllers/Categories/CategoryProtocols.swift
@@ -4,7 +4,7 @@
 
 public protocol CategoryScreenProtocol: StreamableScreenProtocol {
     var topInsetView: UIView { get }
-    var navBarsVisible: Bool? { get }
+    var navBarsVisible: Bool? { get } // nil means "I don't know, check the UINavigationBar"
     func setCategoriesInfo(categoriesInfo: [CategoryCardListView.CategoryInfo], animated: Bool)
     func animateCategoriesList(navBarVisible navBarVisible: Bool)
     func scrollToCategoryIndex(index: Int)
