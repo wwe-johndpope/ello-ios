29287a9310ff14554b5321c985f8ef6b4a408a10
Sean Dougherty <seancdougherty@gmail.com>
2016-11-21 11:59:26 -0700

    Fixes failing snapshot specs

--- Specs/Controllers/IntroViewControllerSpec.swift
+++ Specs/Controllers/IntroViewControllerSpec.swift
@@ -7,28 +7,11 @@ import Quick
 import Nimble
 
 class IntroViewControllerSpec: QuickSpec {
+    
     override func spec() {
         describe("IntroViewController") {
-            var controller = IntroViewController()
-            describe("initialization") {
-                beforeEach {
-                    controller = IntroViewController()
-                }
-
-                it("can be instantiated") {
-                    expect(controller).notTo(beNil())
-                }
-
-                it("is a UIViewController") {
-                    expect(controller).to(beAKindOf(UIViewController.self))
-                }
-
-                it("is a IntroViewController") {
-                    expect(controller).to(beAKindOf(IntroViewController.self))
-                }
-            }
+            var subject: IntroViewController!
             describe("snapshots") {
-                var subject: IntroViewController!
                 beforeEach {
                     subject = IntroViewController()
                 }
