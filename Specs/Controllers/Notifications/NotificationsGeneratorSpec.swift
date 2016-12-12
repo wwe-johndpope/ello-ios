9311cbae5d07aa2ea741088e15cebcc3023e3f7c
Colin T.A. Gray <colinta@gmail.com>
2016-12-12 14:13:43 -0700

    move NotificationsViewControllerSpec into Notifications/ folder

---
+++ Specs/Controllers/Notifications/NotificationsGeneratorSpec.swift
@@ -0,0 +1,17 @@
+////
+///  NotificationsGeneratorSpec.swift
+//
+
+@testable import Ello
+import Quick
+import Nimble
+
+
+class NotificationsGeneratorSpec: QuickSpec {
+    override func spec() {
+        describe("NotificationsGenerator") {
+            it("") {
+            }
+        }
+    }
+}
