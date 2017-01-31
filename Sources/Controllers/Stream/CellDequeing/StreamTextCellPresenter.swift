3eb95c810cb73754ee407e73b431db48c00a6f2b
Colin T.A. Gray <colinta@gmail.com>
2017-01-31 16:43:39 -0700

    prefer guard

--- Sources/Controllers/Stream/CellDequeing/StreamTextCellPresenter.swift
+++ Sources/Controllers/Stream/CellDequeing/StreamTextCellPresenter.swift
@@ -14,34 +14,34 @@ struct StreamTextCellPresenter {
         indexPath: IndexPath,
         currentUser: User?)
     {
-        if let cell = cell as? StreamTextCell {
-            cell.onWebContentReady { webView in
-                if let actualHeight = webView.windowContentSize()?.height, actualHeight != streamCellItem.calculatedCellHeights.webContent {
-                    streamCellItem.calculatedCellHeights.webContent = actualHeight
-                    streamCellItem.calculatedCellHeights.oneColumn = actualHeight
-                    streamCellItem.calculatedCellHeights.multiColumn = actualHeight
-                    postNotification(StreamNotification.UpdateCellHeightNotification, value: cell)
-                }
-            }
+        guard let cell = cell as? StreamTextCell else { return }
 
-            var isRepost = false
-            if let textRegion = streamCellItem.type.data as? TextRegion {
-                isRepost = textRegion.isRepost
-                let content = textRegion.content
-                let html = StreamTextCellHTML.postHTML(content)
-                cell.webView.loadHTMLString(html, baseURL: URL(string: "/"))
+        cell.onWebContentReady { webView in
+            if let actualHeight = webView.windowContentSize()?.height, actualHeight != streamCellItem.calculatedCellHeights.webContent {
+                streamCellItem.calculatedCellHeights.webContent = actualHeight
+                streamCellItem.calculatedCellHeights.oneColumn = actualHeight
+                streamCellItem.calculatedCellHeights.multiColumn = actualHeight
+                postNotification(StreamNotification.UpdateCellHeightNotification, value: cell)
             }
-            // Repost specifics
-            if isRepost == true {
-                cell.leadingConstraint.constant = 30.0
-                cell.showBorder()
-            }
-            else if streamCellItem.jsonable is ElloComment {
-                cell.leadingConstraint.constant = commentMargin
-            }
-            else {
-                cell.leadingConstraint.constant = postMargin
-            }
+        }
+
+        var isRepost = false
+        if let textRegion = streamCellItem.type.data as? TextRegion {
+            isRepost = textRegion.isRepost
+            let content = textRegion.content
+            let html = StreamTextCellHTML.postHTML(content)
+            cell.webView.loadHTMLString(html, baseURL: URL(string: "/"))
+        }
+        // Repost specifics
+        if isRepost == true {
+            cell.leadingConstraint.constant = 30.0
+            cell.showBorder()
+        }
+        else if streamCellItem.jsonable is ElloComment {
+            cell.leadingConstraint.constant = commentMargin
+        }
+        else {
+            cell.leadingConstraint.constant = postMargin
         }
     }
 
