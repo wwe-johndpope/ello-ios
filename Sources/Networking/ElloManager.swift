850b47c77ddf719559048d44b6a85f05c49bf9fe
Colin T.A. Gray <colinta@gmail.com>
2017-02-07 11:04:48 -0700

    just a thought, but on staging I was 'locked out' due to a long API request

--- Sources/Networking/ElloManager.swift
+++ Sources/Networking/ElloManager.swift
@@ -23,6 +23,8 @@ struct ElloManager {
     static var manager: SessionManager {
         let config = URLSessionConfiguration.default
         config.sharedContainerIdentifier = ElloGroupName
+        config.timeoutIntervalForRequest = 10
+        config.timeoutIntervalForResource = 10
         return SessionManager(
             configuration: config,
             serverTrustPolicyManager: ServerTrustPolicyManager(policies: ElloManager.serverTrustPolicies)
