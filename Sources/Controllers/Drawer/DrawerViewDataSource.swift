f96b7ba11ca9f9de9b189276e49381e8415dcf4a
Casey Doran <casey@ello.co>
2017-03-15 17:05:35 -0600

    Adds NFP to the drawer

--- Sources/Controllers/Drawer/DrawerViewDataSource.swift
+++ Sources/Controllers/Drawer/DrawerViewDataSource.swift
@@ -25,8 +25,9 @@ class DrawerViewDataSource: NSObject {
     // moved into a separate function to save compile time
     fileprivate func drawerItems() -> [DrawerItem] {
         return [
-            DrawerItem(name: InterfaceString.Drawer.Store, type: .external("http://ello.threadless.com/")),
             DrawerItem(name: InterfaceString.Drawer.Invite, type: .invite),
+            DrawerItem(name: InterfaceString.Drawer.Magazine, type: .external("https://notforprint.co/")),
+            DrawerItem(name: InterfaceString.Drawer.Store, type: .external("http://ello.threadless.com/")),
             DrawerItem(name: InterfaceString.Drawer.Help, type: .external("https://ello.co/wtf/")),
             DrawerItem(name: InterfaceString.Drawer.Logout, type: .logout),
             DrawerItem(name: InterfaceString.Drawer.Version, type: .version),
