463e5efdf37ca410d35507d55ab80d31879eff17
Colin T.A. Gray <colinta@gmail.com>
2017-03-28 15:13:21 -0600

    truncate the name and username labels

--- Sources/Controllers/Profile/Calculators/ProfileNamesSizeCalculator.swift
+++ Sources/Controllers/Profile/Calculators/ProfileNamesSizeCalculator.swift
@@ -19,19 +19,26 @@ struct ProfileNamesSizeCalculator {
         let nameFont = ProfileNamesView.nameFont
         let usernameFont = ProfileNamesView.usernameFont
 
+        let viewWidth = maxWidth - ProfileNamesView.Size.outerMargins.left - ProfileNamesView.Size.outerMargins.right
+        let maxSize = CGSize(width: viewWidth, height: CGFloat.greatestFiniteMagnitude)
+
         let nameSize: CGSize
         if user.name.isEmpty {
             nameSize = .zero
         }
         else {
-            nameSize = user.name.size(attributes: [
-                NSFontAttributeName: nameFont
-            ]).integral
+            nameSize = user.name.boundingRect(
+                with: maxSize, options: [],
+                attributes: [
+                    NSFontAttributeName: nameFont,
+                ], context: nil).size.integral
         }
 
-        let usernameSize = user.atName.size(attributes: [
-            NSFontAttributeName: usernameFont
-        ]).integral
+        let usernameSize = user.atName.boundingRect(
+            with: maxSize, options: [],
+            attributes: [
+                NSFontAttributeName: usernameFont,
+            ], context: nil).size.integral
 
         let (height, _) = ProfileNamesView.preferredHeight(nameSize: nameSize, usernameSize: usernameSize, maxWidth: maxWidth)
         let totalHeight = height + ProfileNamesView.Size.outerMargins.top + ProfileNamesView.Size.outerMargins.bottom
