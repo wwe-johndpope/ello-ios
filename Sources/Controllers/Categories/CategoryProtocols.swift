274faddf9c406a155f7c5b3654cbb00fbf199535
Colin T.A. Gray <colinta@gmail.com>
2017-04-07 09:44:08 -0600

    reorder protocols for consistency (delegate, then screen protocol)

--- Sources/Controllers/Categories/CategoryProtocols.swift
+++ Sources/Controllers/Categories/CategoryProtocols.swift
@@ -2,6 +2,12 @@
 ///  CategoryProtocols.swift
 //
 
+protocol CategoryScreenDelegate: class {
+    func gridListToggled(sender: UIButton)
+    func categorySelected(index: Int)
+    func searchButtonTapped()
+}
+
 protocol CategoryScreenProtocol: StreamableScreenProtocol {
     var topInsetView: UIView { get }
     var categoryCardsVisible: Bool { get }
@@ -10,10 +16,4 @@ protocol CategoryScreenProtocol: StreamableScreenProtocol {
     func animateCategoriesList(navBarVisible: Bool)
     func scrollToCategory(index: Int)
     func selectCategory(index: Int)
-}
-
-protocol CategoryScreenDelegate: class {
-    func gridListToggled(sender: UIButton)
-    func categorySelected(index: Int)
-    func searchButtonTapped()
 }
