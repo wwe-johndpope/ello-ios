41169f9a6dca71a88ff8597105579f4702e68f87
Colin T.A. Gray <colinta@gmail.com>
2017-04-18 16:20:10 -0600

    update ProfileHeaderCompactView snapshot specs

--- Specs/Controllers/Profile/ProfileHeaderCompactViewSpec.swift
+++ Specs/Controllers/Profile/ProfileHeaderCompactViewSpec.swift
@@ -16,32 +16,24 @@ class ProfileHeaderCompactViewSpec: QuickSpec {
             let statsHeight: CGFloat = ProfileStatsView.Size.height
             let namesHeight: CGFloat = 75
 
-            let defauntBioHeight: CGFloat = 50
+            let defaultBioHeight: CGFloat = 50
             let defaultLocationHeight: CGFloat = ProfileLocationView.Size.height
             let defaultLinksHeight: CGFloat = 100
             let defaultCountHeight: CGFloat = ProfileTotalCountView.Size.height
 
-            let expectations: [(String, bioHeight: CGFloat, locationHeight: CGFloat, linksHeight: CGFloat, countHeight: CGFloat, hasBadges: Bool)] = [
-                ("no bio", bioHeight: 0, locationHeight: defaultLocationHeight, linksHeight: defaultLinksHeight, countHeight: defaultCountHeight, hasBadges: true),
-                ("no links", bioHeight: defauntBioHeight, locationHeight: defaultLocationHeight, linksHeight: 0, countHeight: defaultCountHeight, hasBadges: true),
-                ("no location", bioHeight: defauntBioHeight, locationHeight: 0, linksHeight: defaultLinksHeight, countHeight: defaultCountHeight, hasBadges: true),
-                ("no badges", bioHeight: defauntBioHeight, locationHeight: 0, linksHeight: defaultLinksHeight, countHeight: defaultCountHeight, hasBadges: false),
-                ("no bio or links", bioHeight: 0, locationHeight: defaultLocationHeight, linksHeight: 0, countHeight: defaultCountHeight, hasBadges: true),
-                ("no bio, no links, no location", bioHeight: 0, locationHeight: 0, linksHeight: 0, countHeight: defaultCountHeight, hasBadges: true),
-                ("no bio, no links, no location, no total count", bioHeight: 0, locationHeight: 0, linksHeight: 0, countHeight: 0, hasBadges: true),
-                ("no bio or no location", bioHeight: 0, locationHeight: 0, linksHeight: defaultLinksHeight, countHeight: defaultCountHeight, hasBadges: true)
+            let expectations: [(String, bioHeight: CGFloat, locationHeight: CGFloat, linksHeight: CGFloat, hasBadges: Bool)] = [
+                ("no bio", bioHeight: 0, locationHeight: defaultLocationHeight, linksHeight: defaultLinksHeight, hasBadges: true),
+                ("no links", bioHeight: defaultBioHeight, locationHeight: defaultLocationHeight, linksHeight: 0, hasBadges: true),
+                ("no location", bioHeight: defaultBioHeight, locationHeight: 0, linksHeight: defaultLinksHeight, hasBadges: true),
+                ("no badges", bioHeight: defaultBioHeight, locationHeight: 0, linksHeight: defaultLinksHeight, hasBadges: false),
+                ("no bio, no links", bioHeight: 0, locationHeight: defaultLocationHeight, linksHeight: 0, hasBadges: true),
+                ("no bio, no links, no location", bioHeight: 0, locationHeight: 0, linksHeight: 0, hasBadges: true),
+                ("no bio, no links, no location, no badges", bioHeight: 0, locationHeight: 0, linksHeight: 0, hasBadges: false),
+                ("no bio, no location", bioHeight: 0, locationHeight: 0, linksHeight: defaultLinksHeight, hasBadges: true)
             ]
 
-            for (desc, bioHeight, locationHeight, linksHeight, countHeight, hasBadges) in expectations {
+            for (desc, bioHeight, locationHeight, linksHeight, hasBadges) in expectations {
                 it("\(desc) profile header renders correctly") {
-                    let totalViewsCount: AnyObject
-                    if countHeight == 0 {
-                        totalViewsCount = "" as AnyObject // stubs will turn this into nil
-                    }
-                    else {
-                        totalViewsCount = 1 as AnyObject
-                    }
-
                     let user: User = stub([
                         "name" : "bob",
                         "username" : "bill",
@@ -50,10 +42,9 @@ class ProfileHeaderCompactViewSpec: QuickSpec {
                         "followersCount" : 444,
                         "lovesCount" : 89,
                         "formattedShortBio" : "This is a bio",
-                        "location" : "Denver",
-                        "totalViewsCount" : totalViewsCount,
-                        "categories": [Ello.Category.stub([:])],
-                        "externalLinksList" : [["url" : "http://google.com", "text" : "google"]],
+                        "location" : locationHeight > 0 ? "Denver" : "",
+                        "totalViewsCount" : 1,
+                        "externalLinksList" : linksHeight > 0 ? [["url" : "http://google.com", "text" : "google"]] : [],
                     ])
 
                     if hasBadges {
@@ -63,15 +54,15 @@ class ProfileHeaderCompactViewSpec: QuickSpec {
                     let cell: ProfileHeaderCell = ProfileHeaderCell()
                     let item: StreamCellItem = StreamCellItem(jsonable: user, type: .profileHeader)
                     item.calculatedCellHeights.profileAvatar = avatarHeight
-                    item.calculatedCellHeights.profileTotalCount = countHeight
-                    item.calculatedCellHeights.profileBadges = hasBadges ? countHeight : 0
+                    item.calculatedCellHeights.profileTotalCount = defaultCountHeight
+                    item.calculatedCellHeights.profileBadges = hasBadges ? defaultCountHeight : 0
                     item.calculatedCellHeights.profileNames = namesHeight
                     item.calculatedCellHeights.profileStats = statsHeight
                     item.calculatedCellHeights.profileBio = bioHeight
                     item.calculatedCellHeights.profileLocation = locationHeight
                     item.calculatedCellHeights.profileLinks = linksHeight
 
-                    let totalHeight = avatarHeight + namesHeight + statsHeight + bioHeight + locationHeight + linksHeight + countHeight
+                    let totalHeight = avatarHeight + namesHeight + statsHeight + bioHeight + locationHeight + linksHeight + defaultCountHeight
                     let size = CGSize(width: width, height: totalHeight)
                     cell.frame.size = size
                     // we need to force the cell to layout so that view bounds are calculated before configure is called
