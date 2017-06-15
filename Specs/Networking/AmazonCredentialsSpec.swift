28e01c7c3d1fc55874c6f6aefc8b2a23a39d7eed
Colin T.A. Gray <colinta@gmail.com>
2017-06-15 13:46:40 +0100

    update AmazonCredentialsSpecs

--- Specs/Networking/AmazonCredentialsSpec.swift
+++ Specs/Networking/AmazonCredentialsSpec.swift
@@ -9,38 +9,35 @@ import Moya
 
 
 class AmazonCredentialsSpec: QuickSpec {
-    var credentials : AmazonCredentials?
-
     override func spec() {
         describe("requesting credentials") {
             describe("requesting an AmazonCredentials object") {
+                var credentials : AmazonCredentials?
                 beforeEach() {
-                    let endpoint = ElloAPI.amazonCredentials
-                    self.credentials = nil
-                    ElloProvider.shared.elloRequest(endpoint,
-                                    success: { credentialsData, responseConfig in
-                            if let credentials = credentialsData as? AmazonCredentials {
-                                self.credentials = credentials
-                            }
-                        })
+                    credentials = nil
+                    ElloProvider.shared.request(.amazonCredentials)
+                        .then { response in
+                            credentials = response.0 as? AmazonCredentials
+                        }
                 }
+
                 it("should not be nil") {
-                    expect(self.credentials).toNot(beNil())
+                    expect(credentials).toNot(beNil())
                 }
                 it("should set the prefix") {
-                    expect(self.credentials!.prefix).to(equal("uploads/prefix"))
+                    expect(credentials!.prefix).to(equal("uploads/prefix"))
                 }
                 it("should set the policy") {
-                    expect(self.credentials!.policy).to(equal("prolicy-hash"))
+                    expect(credentials!.policy).to(equal("prolicy-hash"))
                 }
                 it("should set the signature") {
-                    expect(self.credentials!.signature).to(equal("signature-hash"))
+                    expect(credentials!.signature).to(equal("signature-hash"))
                 }
                 it("should set the endpoint") {
-                    expect(self.credentials!.endpoint).to(equal("https://endpoint.amazonaws.com"))
+                    expect(credentials!.endpoint).to(equal("https://endpoint.amazonaws.com"))
                 }
                 it("should set the access_key") {
-                    expect(self.credentials!.accessKey).to(equal("access-key"))
+                    expect(credentials!.accessKey).to(equal("access-key"))
                 }
             }
         }
