5d82207ca7c91df3805c6c1e2e078ae40c489b26
Colin T.A. Gray <colinta@gmail.com>
2017-08-01 14:09:42 -0600

    use 'NestedTabBarView' instead of lots of code in HomeScreenNavBar

--- Sources/Controllers/Home/HomeScreen.swift
+++ Sources/Controllers/Home/HomeScreen.swift
@@ -29,12 +29,7 @@ protocol HomeScreenNavBar: class {
 }
 
 struct HomeScreenNavBarSize {
-    static let margins: CGFloat = 15
-    static let tabSpacing: CGFloat = 1
-    static let logoButtonMargin: CGFloat = 5
     static let typeOffset: CGFloat = 18.625
-    static let buttonHeight: CGFloat = 40
-    static let lineThickness: CGFloat = 1
 }
 
 fileprivate typealias Size = HomeScreenNavBarSize
@@ -61,130 +56,57 @@ extension HomeScreenNavBar {
 
     func arrangeHomeScreenNavBar(type: HomeScreenType, navigationBar: UIView) {
         let logoButton = UIButton()
-        navigationBar.addSubview(logoButton)
         logoButton.setImage(.elloType, imageStyle: .normal, for: .normal)
         logoButton.addTarget(self, action: #selector(homeScreenScrollToTop), for: .touchUpInside)
 
-        let editorialsButton = StyledButton()
-        editorialsButton.setTitle(InterfaceString.Editorials.NavbarTitle, for: .normal)
-        navigationBar.addSubview(editorialsButton)
+        let tabBar = NestedTabBarView()
+        let editorialsTab = tabBar.createTab(title: InterfaceString.Editorials.NavbarTitle)
+        let otherTab = tabBar.createTab()
+        let middleTab = tabBar.createTab(title: InterfaceString.ArtistInvites.Title)
 
-        let editorialsLine = UIView()
-        editorialsButton.addSubview(editorialsLine)
+        editorialsTab.addTarget(self, action: #selector(homeScreenEditorialsTapped))
+        middleTab.addTarget(self, action: #selector(homeScreenArtistInvitesTapped))
 
-        let otherButton = StyledButton()
-        navigationBar.addSubview(otherButton)
-
-        let otherLine = UIView()
-        otherButton.addSubview(otherLine)
-
-        let middleButton = StyledButton()
-        middleButton.setTitle(InterfaceString.ArtistInvites.Title, for: .normal)
-        let middleLine = UIView()
+        tabBar.addTab(editorialsTab)
+        if type.tabCount == 3 {
+            tabBar.addTab(middleTab)
+        }
+        tabBar.addTab(otherTab)
 
         switch type {
         case let .editorials(loggedIn):
-            editorialsButton.style = .clearBlack
-            editorialsLine.backgroundColor = .black
-
-            otherButton.style = .clearGray
-            otherLine.backgroundColor = .greyA
+            tabBar.select(tab: editorialsTab)
 
             if loggedIn {
-                otherButton.setTitle(InterfaceString.Following.Title, for: .normal)
-                otherButton.addTarget(self, action: #selector(homeScreenFollowingTapped), for: .touchUpInside)
+                otherTab.title = InterfaceString.Following.Title
+                otherTab.addTarget(self, action: #selector(homeScreenFollowingTapped))
             }
             else {
-                otherButton.setTitle(InterfaceString.Discover.Title, for: .normal)
-                otherButton.addTarget(self, action: #selector(homeScreenDiscoverTapped), for: .touchUpInside)
+                otherTab.title = InterfaceString.Discover.Title
+                otherTab.addTarget(self, action: #selector(homeScreenDiscoverTapped))
             }
-
-            middleButton.style = .clearGray
-            middleButton.addTarget(self, action: #selector(homeScreenArtistInvitesTapped), for: .touchUpInside)
-            middleLine.backgroundColor = .greyA
         case .following:
-            editorialsButton.style = .clearGray
-            editorialsButton.addTarget(self, action: #selector(homeScreenEditorialsTapped), for: .touchUpInside)
-            editorialsLine.backgroundColor = .greyA
-
-            otherButton.style = .clearBlack
-            otherButton.setTitle(InterfaceString.Following.Title, for: .normal)
-            otherLine.backgroundColor = .black
-
-            middleButton.style = .clearGray
-            middleButton.addTarget(self, action: #selector(homeScreenArtistInvitesTapped), for: .touchUpInside)
-            middleLine.backgroundColor = .greyA
+            tabBar.select(tab: otherTab)
+            otherTab.title = InterfaceString.Following.Title
         case .artistInvites:
-            editorialsButton.style = .clearGray
-            editorialsButton.addTarget(self, action: #selector(homeScreenEditorialsTapped), for: .touchUpInside)
-            editorialsLine.backgroundColor = .greyA
-
-            otherButton.style = .clearGray
-            otherButton.setTitle(InterfaceString.Following.Title, for: .normal)
-            otherButton.addTarget(self, action: #selector(homeScreenFollowingTapped), for: .touchUpInside)
-            otherLine.backgroundColor = .greyA
-
-            middleButton.style = .clearBlack
-            middleLine.backgroundColor = .black
+            tabBar.select(tab: middleTab)
+            otherTab.title = InterfaceString.Following.Title
+            otherTab.addTarget(self, action: #selector(homeScreenFollowingTapped))
         case .discover:
-            editorialsButton.style = .clearGray
-            editorialsButton.addTarget(self, action: #selector(homeScreenEditorialsTapped), for: .touchUpInside)
-            editorialsLine.backgroundColor = .greyA
-
-            otherButton.style = .clearBlack
-            otherButton.setTitle(InterfaceString.Discover.Title, for: .normal)
-            otherLine.backgroundColor = .black
+            tabBar.select(tab: otherTab)
+            otherTab.title = InterfaceString.Discover.Title
         }
+
+        navigationBar.addSubview(logoButton)
+        navigationBar.addSubview(tabBar)
 
         logoButton.snp.makeConstraints { make in
             make.centerX.equalTo(navigationBar)
             make.top.equalTo(navigationBar).offset(BlackBar.Size.height + HomeScreenNavBarSize.typeOffset)
         }
 
-        if type.tabCount == 3 {
-            navigationBar.addSubview(middleButton)
-            middleButton.addSubview(middleLine)
-        }
-
-        editorialsButton.snp.makeConstraints { make in
-            make.leading.equalTo(navigationBar).inset(Size.margins)
-            make.top.equalTo(logoButton.snp.bottom).offset(Size.logoButtonMargin)
-            make.height.equalTo(Size.buttonHeight)
-            make.width.equalTo(otherButton)
-        }
-        editorialsLine.snp.makeConstraints { make in
-            make.leading.trailing.bottom.equalTo(editorialsButton)
-            make.height.equalTo(Size.lineThickness)
-        }
-
-        if type.tabCount == 3 {
-            middleButton.snp.makeConstraints { make in
-                make.leading.equalTo(editorialsButton.snp.trailing).offset(Size.tabSpacing)
-                make.top.equalTo(logoButton.snp.bottom).offset(Size.logoButtonMargin)
-                make.height.equalTo(Size.buttonHeight)
-                make.width.equalTo(otherButton)
-            }
-            middleLine.snp.makeConstraints { make in
-                make.leading.trailing.bottom.equalTo(middleButton)
-                make.height.equalTo(Size.lineThickness)
-            }
-        }
-
-        otherButton.snp.makeConstraints { make in
-            make.trailing.equalTo(navigationBar).inset(Size.margins)
-            make.top.equalTo(logoButton.snp.bottom).offset(Size.logoButtonMargin)
-            make.height.equalTo(Size.buttonHeight)
-
-            if type.tabCount == 2 {
-                make.leading.equalTo(editorialsButton.snp.trailing).offset(Size.tabSpacing)
-            }
-            else {
-                make.leading.equalTo(middleButton.snp.trailing).offset(Size.tabSpacing)
-            }
-        }
-        otherLine.snp.makeConstraints { make in
-            make.leading.trailing.bottom.equalTo(otherButton)
-            make.height.equalTo(Size.lineThickness)
+        tabBar.snp.makeConstraints { make in
+            make.leading.trailing.bottom.equalTo(navigationBar)
         }
     }
 }
---
+++ Sources/Views/NestedTabBarView.swift
@@ -0,0 +1,108 @@
+////
+///  NestedTabBarView.swift
+//
+
+class NestedTabBarView: View {
+    struct Size {
+        static let height: CGFloat = 60
+        static let tabSpacing: CGFloat = 1
+        static let margins = UIEdgeInsets(top: 5, left: 15, bottom: 15, right: 15)
+        static let buttonHeight: CGFloat = 40
+        static let lineThickness: CGFloat = 1
+    }
+
+    class Tab {
+        fileprivate let button = StyledButton(style: .clearGray)
+        fileprivate let line = UIView()
+
+        static func == (lhs: Tab, rhs: Tab) -> Bool {
+            return lhs.button == rhs.button
+        }
+
+        init(title: String? = nil) {
+            button.addSubview(line)
+            button.setTitle(title, for: .normal)
+        }
+
+        var title: String? {
+            get { return button.title(for: .normal) }
+            set { button.setTitle(newValue, for: .normal) }
+        }
+
+        func addTarget(_ target: Any?, action: Selector) {
+            button.addTarget(target, action: action, for: .touchUpInside)
+        }
+    }
+
+    var tabs: [Tab] = []
+    var isArranged = false
+
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: UIViewNoIntrinsicMetric, height: Size.height)
+    }
+
+    func createTab(title: String? = nil) -> Tab {
+        return Tab(title: title)
+    }
+
+    func addTab(_ tab: Tab) {
+        guard !isArranged else {
+            fatalError("cannot add tabs after tabbar has been added to a view")
+        }
+
+        addSubview(tab.button)
+        tabs.append(tab)
+    }
+
+    func select(tab selectedTab: Tab) {
+        for tab in tabs {
+            if tab == selectedTab {
+                tab.button.style = .clearBlack
+                tab.line.backgroundColor = .black
+                tab.button.isUserInteractionEnabled = false
+            }
+            else {
+                tab.button.style = .clearGray
+                tab.line.backgroundColor = .greyA
+                tab.button.isUserInteractionEnabled = true
+            }
+        }
+    }
+
+    override func didMoveToSuperview() {
+        super.didMoveToSuperview()
+        if superview != nil {
+            rearrange()
+        }
+    }
+
+    fileprivate func rearrange() {
+        guard !isArranged else { return }
+
+        tabs.eachPair { prevTab, tab, isLast in
+            tab.button.snp.makeConstraints { make in
+                make.top.equalTo(self).inset(Size.margins)
+                make.height.equalTo(Size.buttonHeight)
+
+                if let prevTab = prevTab {
+                    make.leading.equalTo(prevTab.button.snp.trailing).offset(Size.tabSpacing)
+                    make.width.equalTo(prevTab.button)
+                }
+                else {
+                    make.leading.equalTo(self).inset(Size.margins)
+                }
+
+                if isLast {
+                    make.trailing.equalTo(self).inset(Size.margins)
+                }
+            }
+
+            tab.line.snp.makeConstraints { make in
+                make.leading.trailing.bottom.equalTo(tab.button)
+                make.height.equalTo(Size.lineThickness)
+            }
+        }
+
+        isArranged = true
+    }
+}
