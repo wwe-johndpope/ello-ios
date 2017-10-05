2899acb3b9879fd4210860caaff343003357b2a3
Colin T.A. Gray <colinta@gmail.com>
2017-10-05 15:20:08 -0600

    adds InterfaceStrings

--- Specs/Utilities/InterfaceStringSpec.swift
+++ Specs/Utilities/InterfaceStringSpec.swift
@@ -9,14 +9,17 @@ import Nimble
 
 class InterfaceStringSpec: QuickSpec {
     override func spec() {
-        fdescribe("InterfaceString") {
+        describe("InterfaceString") {
             describe("ArtistInvites") {
                 let expectations: [(h: Int, m: Int, s: Int, String)] = [
                     (h: 40, m: 52, s: 31, "40:52:31 Remaining"),
+                    (h: 10, m: 0, s: 0, "10:00:00 Remaining"),
+                    (h: 0, m: 10, s: 0, "00:10:00 Remaining"),
+                    (h: 0, m: 0, s: 10, "00:00:10 Remaining"),
                     (h: 2, m: 3, s: 1, "02:03:01 Remaining"),
                 ]
                 for (h, m, s, expected) in expectations {
-                    let seconds = h * 86400 + m * 60 + s
+                    let seconds = h * 3600 + m * 60 + s
                     it("formats Countdown(\(seconds))") {
                         expect(InterfaceString.ArtistInvites.Countdown(seconds)) == expected
                     }
