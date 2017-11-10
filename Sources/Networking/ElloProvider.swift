e6eea71e1a15565acc9b978f482493ce29d38a0d
Colin T.A. Gray <colinta@gmail.com>
2017-11-10 14:09:53 -0700

    refactor/clean up 'ResponseConfig' code

--- Sources/Networking/ElloProvider.swift
+++ Sources/Networking/ElloProvider.swift
@@ -6,6 +6,7 @@ import Moya
 import Result
 import Alamofire
 import PromiseKit
+import WebLinking
 
 
 typealias ElloRequestClosure = (target: ElloAPI, success: ElloSuccessCompletion, failure: ElloFailureCompletion)
@@ -434,12 +435,21 @@ extension ElloProvider {
 
     private func parseResponse(_ response: HTTPURLResponse?) -> ResponseConfig {
         let config = ResponseConfig()
-        config.statusCode = response?.statusCode
-        config.lastModified = response?.allHeaderFields["Last-Modified"] as? String
-        config.totalPages = response?.allHeaderFields["X-Total-Pages"] as? String
-        config.totalCount = response?.allHeaderFields["X-Total-Count"] as? String
-        config.totalPagesRemaining = response?.allHeaderFields["X-Total-Pages-Remaining"] as? String
 
-        return parseLinks(response, config: config)
+        if let response = response {
+            config.statusCode = response.statusCode
+            config.lastModified = response.allHeaderFields["Last-Modified"] as? String
+            config.totalPages = response.allHeaderFields["X-Total-Pages"] as? String
+            config.totalCount = response.allHeaderFields["X-Total-Count"] as? String
+            config.totalPagesRemaining = response.allHeaderFields["X-Total-Pages-Remaining"] as? String
+            config.nextQuery = parseNextLink(response, config: config)
+        }
+
+        return config
     }
+
+    private func parseNextLink(_ response: HTTPURLResponse, config: ResponseConfig) -> URLComponents? {
+        return response.findLink(relation: "next").flatMap { URLComponents(string: $0.uri) }
+    }
+
 }
